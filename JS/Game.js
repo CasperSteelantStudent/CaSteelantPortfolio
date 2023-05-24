540dd0d26980ffcff62d22400a28776af7ad3f67
Casteelant <caspersteelant@gmail.com>
2023-05-24 21:31:21 +0200

    Finalising P5JS game

--- JS/Game.js
+++ JS/Game.js
@@ -5,7 +5,9 @@ let velX = 5,
     velY = 5; // velocity of the object
 let angle = 0; // rotation angle of the object
 let rotationSpeed = 0.1; // speed of rotation
-let Score = 0;
+let Score = 0;      //The score equals the amount of bombs that were transformed
+let H2G2;
+let quotes; // Array to store the quotes
 
 class Enemy {
     constructor(posX, posY, velX, velY, size, color) {
@@ -21,27 +23,27 @@ class Enemy {
     }
 
     update() {
-        this.posX += this.velX;
+        this.posX += this.velX;                     //makes the enemies move around the screen
         this.posY += this.velY;
-        if (this.posX < this.size / 2 || this.posX > width - this.size / 2) {
+        if (this.posX < this.size / 2 || this.posX > width - this.size / 2) {  //if the enemy reaches the edge of the screen, invert the direction of either x or y
             this.velX *= -1;
         }
         if (this.posY < this.size / 2 || this.posY > height - this.size / 2) {
             this.velY *= -1;
         }
-        this.angle += this.rotationSpeed;
+        this.angle += this.rotationSpeed;       //makes the enemies rotate
     }
 
 
     display() {
 
-        push();
-        if (this.isCube) {
+        push(); //remembers the object before any translations or rotations
+        if (this.isCube) {          //checks wether or not the object has been clicked or not, displays either a bomb or a flower pot
             stroke(10);
-            translate(this.posX, this.posY);
-            rotate(this.angle);
+            translate(this.posX, this.posY); //continually moves the object around
+            rotate(this.angle);     //continually rotates the object
             //flowers
-            scale(0.7);
+            scale(0.7);     //scales the entire flower
             fill(this.color);
             rect(-this.size / 2, -this.size / 2, this.size, this.size);
             rect(-this.size / 2 - 3, -this.size / 2 - 3, this.size + 6, 10);
@@ -49,14 +51,14 @@ class Enemy {
             fill(255, 0, 255, 240);
             translate(0, -60);
             noStroke();
-            for (var r1 = 0; r1 < 10; r1++) {
+            for (var r1 = 0; r1 < 10; r1++) {       //a loop that creates ellipses to emulate flower petals
                 ellipse(0, 10, 25, 50);
                 rotate(PI / 5);
             }
             fill(218, 165, 32);
-            circle(0, 0, 20);
+            circle(0, 0, 20);       //center of the flower
         } else {
-            // Draw square
+            // Draw bomb
             translate(this.posX, this.posY);
             rotate(this.angle);
             fill(0);
@@ -80,13 +82,13 @@ class Enemy {
 
     onClick(x, y) {
         if (this.isClicked(x, y)) {
-            if (!this.isCube) {
-                transformsound.play();
-                this.isCube = !this.isCube;
-                Score += 1;
-                let energyBall = new EnergyBall(x, y);
+            if (!this.isCube) {         //checks wether or not the coordinates sent by mouseClick are within the boundaries of the bomb, and changes isCube to trigger the other shape
+                transformsound.play();      //plays a sound effect
+                this.isCube = !this.isCube;     //changes the isCube to true (shouldn't really be cube anymore, might rename)
+                Score += 1;         //adds 1 to the score
+                let energyBall = new EnergyBall(x, y);      //creates a new energy ball to hide the switching of sprites
                 energyBalls.push(energyBall);
-                
+
             }
         }
     }
@@ -99,8 +101,6 @@ class Robot {
         this.x = posX - 200;
         this.y = posY - 100;
     }
-
-
 
     display() {
         stroke(0);
@@ -161,23 +161,32 @@ class Robot {
         fill(255);
         ellipse(200, 100, 120, 120);
 
-        translate(-(this.posX - 200), -(this.posY - 100));
-        // Draw eyes
+        if (Score == floor(amount)) {  //The score equals the amount of bombs inside the Enemies array
+            textAlign(LEFT);
+            text("it's lying, you've only got " + Score, 300, 100);         //displays the actual score instead of the joke score
+        } else if (millis() >= amount * 1000) {       //Game Over trigger when time runs out
+            textAlign(LEFT);
+            text(quotes.getColumn(column), 300,100);
+        }
 
-        let x1 = constrain(mouseX, (this.posX - 20), (this.posX + 20));
+
+        translate(-(this.posX - 200), -(this.posY - 100));      //had to translate the eyes seperately 
+
+        let x1 = constrain(mouseX, (this.posX - 20), (this.posX + 20));     //constrains the position of the eyes between a box, while still trying to follow the cursor
         let y = constrain(mouseY, (this.posY - 20), (this.posY + 20));
 
         fill(0, 255, 0);
         for (let i = -1; i <= 1; i += 2) {
-            triangle(x1 + i * 40, y, x1 + i * 12, y, x1 + i * 20, y + 9);
+            triangle(x1 + i * 40, y, x1 + i * 12, y, x1 + i * 20, y + 9);   // Draw eyes, i makes the eyes -get drawn twice 
         }
+
+
     }
 }
 
-
 class EnergyBall {
     constructor(x, y) {
-        this.x = x;
+        this.x = x;                     //same x and y used by mouseClicked 
         this.y = y;
         this.radius = 50;
         this.easing = 0.05;
@@ -185,24 +194,32 @@ class EnergyBall {
 
     update() {
         let targetRadius = 0;
-        this.radius = lerp(this.radius, targetRadius, this.easing);
+        this.radius = lerp(this.radius, targetRadius, this.easing);     //makes the radius slowly shrink with a sine ease
     }
 
-    display() {
+    display() {         //displays the energy ball with the shrinking radius
         noStroke();
         fill(0, 255, 0);
         ellipse(this.x, this.y, this.radius * 2, this.radius * 2);
     }
 }
 
+function preload() {
+    // Load the CSV file
+    quotes = loadTable('Media/quotes.csv', 'csv');
+}
+
 function setup() {
+    column = floor(random(0,quotes.getColumnCount()));  //gets a random column from the .csv file
+    H2G2 = loadFont('Fonts/Tw Cen MT Std Ultra Bold.otf');
     transformsound = loadSound('Media/transform.wav');
+    timeStart = millis();           //keeps track of how long the player has been playing the game
     createCanvas(600, 400);
     textAlign(CENTER);
     textSize(20);
-    robot = new Robot(random(200, 400), random(100, 300));
+    robot = new Robot(200, 200);
     amount = random(5, 10);
-    for (let i = 0; i <= amount - 1; i++) {
+    for (let i = 0; i <= amount - 1; i++) {                     //creates an amount of enemies equal to the amount variable
         Enemies[i] = new Enemy();
         Enemies[i].posX = random(100, 500);
         Enemies[i].posY = random(100, 300);
@@ -218,8 +235,17 @@ function draw() {
     push();
     if (Score == floor(amount)) {  //The score equals the amount of bombs inside the Enemies array
         robot.display();            //gotta keep the robot on the screen, consistency or something
-        text('YOU LIVE!', 300, 170);   //positive affirmation and all that
-        text('score: 42', 300, 190);  //If I were to implement an actual timer, it'd probably just keep track of the time that has passed and detract that from the maximum score, but 42 is funnier
+        textAlign(CENTER);
+        text('YOU LIVE!', 300, 80);   //positive affirmation and all that
+        text('score: 42', 300, 100);  //If I were to implement an actual timer, it'd probably just keep track of the time that has passed and detract that from the maximum score, but 42 is funnier
+        return;
+    } else if (millis() >= amount * 1000) {     //game over when the tiemr reaches a set amount, depends on how many bombs are present
+        robot.display();
+        textAlign(CENTER);
+        fill(255, 0, 0);
+        textStyle(BOLD);
+        noStroke();
+        text('YOU DIED!', 300, 80);   //tells the user he died
     }
     else {
         robot.display();
@@ -227,23 +253,26 @@ function draw() {
             Enemies[i].update();
             Enemies[i].display();
         }
-        for (let i = 0; i < energyBalls.length; i++) {
+        for (let i = 0; i < energyBalls.length; i++) {      //this displays and updates all energy ball objects that are created via the mouseClick condition and the energyball class
             let energyBall = energyBalls[i];
             energyBall.update();
             energyBall.display();
         }
         fill(0);
         noStroke();
+        textAlign(CENTER);
         text("you're being targeted by bombs", 300, 70);
         text("disarm the bombs by clicking on them", 300, 130);
         fill(255, 0, 0);
         textStyle(BOLD);
-        text("DON'T PANIC!", 300, 100);
+        textFont(H2G2);
+        stroke(255, 255, 0);
+        text("DON'T PANIC!", 300, 100);         //prevents the user from panicking
     }
     pop();
 }
 
-function mouseClicked() {
+function mouseClicked() {                       //when the mouse clicks on the screen, let the enemies class decide wether or not the mouse coords align with any of its objects
     for (let i = 0; i < amount; i++) {
         Enemies[i].onClick(mouseX, mouseY);
     }
